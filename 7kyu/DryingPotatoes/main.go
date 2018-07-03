e412c75053dd1815fc44c7bb3888dba9b397e0f8
Dale Moore <dale@dmdigital.net.au>
2018-07-03 23:47:02 +1000

    wip

--- 7kyu/DryingPotatoes/main.go
+++ 7kyu/DryingPotatoes/main.go
@@ -1,10 +1,7 @@
 package kata_drying_potatoes
 
+import "math"
+
 func Potatoes(p0, w0, p1 int) int {
-	var dryWeight = p0 - 99 * (w0 / 100)
-	var waterWeight = 99 * (w0 / 100)
-
-	50 =
-
-	return
+	return int(math.Floor((float64(w0) * (100 - float64(p1))) / (100 - float64(p0))))
 }
